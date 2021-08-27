d90f0636c3e09a7774c103ffc1ba14a27b3ef235
aharris52 <aharris@mail.greenriver.edu>
2021-08-27 14:02:29 -0700

    Brief introduction to Swift closures.

---
+++ Swift Closures/Swift Closures/main.swift
@@ -0,0 +1,61 @@
+//
+//  main.swift
+//  Swift Closures
+//
+//  This console program is for better understanding
+//  the syntax and application of closures in the
+//  the Swift programming language.
+//
+//Each statemnt produces the same output. Each satement
+//is made even more succinct as you go down the page.
+//
+//  Created by Andrew Harris on 8/27/21.
+//
+
+import Foundation
+
+func calculator(n1: Int, n2: Int, operation: (Int, Int) -> Int) -> Int {
+    return operation(n1, n2)
+}
+
+func add(no1: Int, no2: Int) -> Int {
+    return no1 + no2
+}
+
+print("The following output is created using different syntax for each output.")
+print("This is so that we can more easily see the changes in syntax and the ")
+print("the trade-offs in readability that come with brevity.")
+print()
+print(calculator(n1: 2, n2: 3, operation: add(no1:no2:)))
+
+//  FIRST WAY:
+//  the same f(x) converted to a closure:
+//
+//  {(no1: Int, no2: Int) -> Int in
+//      return no1 + no2
+//  }
+print(calculator(n1: 2, n2: 3, operation: {(no1: Int, no2: Int) -> Int in
+    return no1 + no2
+}))
+
+//  SECOND WAY:
+//  Using type inference we can eliminate the
+//  data type declarations in our expression.
+//  We can also remove the return key word in
+//  our closures, further abbreviating our code.
+//
+//{(no1, no2) in
+//      no1 + no2
+//}
+print(calculator(n1: 2, n2: 3, operation: { (no1, no2) in no1 + no2
+}))
+ 
+// THIRD WAY:
+//  We can also utilize anonymous parameter names
+//  using the $0, $1, ...$n
+print(calculator(n1: 2, n2: 3, operation: { $0 + $1 }))
+
+
+//  FOURTH WAY:
+//  We can abbreviate even further by using a trailing closure.
+print(calculator(n1: 2, n2: 3) { $0 + $1 })
