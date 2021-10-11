63927c54036c41071096599c332c843aeef37142
aharris52 <aharris@mail.greenriver.edu>
2021-10-11 12:31:00 -0700

    Created demo project to process images stored on device.

---
+++ Image Picker Demo/Image Picker Demo/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  Image Picker Demo
+//
+//  Created by Andrew Harris on 10/11/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var imageView: UIImageView!
+    @IBAction func didTapButton(_ sender: Any) {
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
