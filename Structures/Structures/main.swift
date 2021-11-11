24fba7ce5ab49484ebf59b767538c2fb4eb0ba9b
aharris52 <aharris@mail.greenriver.edu>
2021-11-10 16:54:47 -0800

    Created project to learn more about structs in swift.

---
+++ Structures/Structures/main.swift
@@ -0,0 +1,43 @@
+//
+//  main.swift
+//  Structures
+//
+//  Created by Andrew Harris on 11/10/21.
+//
+
+// "Structures and classes are good choices for
+// storing data and modeling behavior in your apps,
+// but their similarities can make it difficult
+// [for FNGs] to choose one over the other."
+// - developer.apple.com
+//
+// Apple gives the following guidance on choosing
+// between the two:
+//
+// 1) CHOOSE STRUCTURES BY DEFAULT
+// 2) Use classes if you need Obj-C
+//    interoperability.
+// 3) Use classes when you need control
+//    of the identity of the data you're
+//    modeling.
+// 4) Use structures along with protocols
+//    to adopt behavior by sharing implementations.
+//    Composition over Inheritance
+// 5) See item 1
+
+import Foundation
+
+struct Town {
+    
+    let name = "Crazy Town"
+    var citizens = ["Andrew", "August"]
+    var resources = ["par-Lights": 20, "boom-mics": 2, "photometers": 2, "followers": 113000]
+}
+
+var myTown = Town()
+
+print(myTown.name)
+print(myTown.citizens)
+print("\(myTown.name) has \(myTown.resources["followers"]!) followers.")
+myTown.citizens.append("Denise")
+print(myTown.citizens.count)
