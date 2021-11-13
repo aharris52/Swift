ff3128bfaae4bfeb5a331f1c2c03f03dde57ce03
aharris52 <aharris@mail.greenriver.edu>
2021-11-12 16:53:42 -0800

    Project to better understand the properties of structures.

--- Structures/Structures/main.swift
+++ Structures/Structures/main.swift
@@ -27,17 +27,38 @@
 
 import Foundation
 
+// This struct is very rigid, and would
+// present problems as the surrounding
+// environment grew around it.
 struct Town {
-    
+    // Properties
     let name = "Crazy Town"
     var citizens = ["Andrew", "August"]
     var resources = ["par-Lights": 20, "boom-mics": 2, "photometers": 2, "followers": 113000]
+
+    // Behavior
+    func film() {
+        print("Lights, camera, action!")
+    }
+}
+
+
+struct NewTown {
+    let name: String
+    var citizens: [String]
+    var resources: [String: Int]
+    
+    init(townName: String, people: [String], stats: [String: Int]) {
+        name = townName
+        citizens = people
+        resources = stats
+    }
 }
 
 var myTown = Town()
-
 print(myTown.name)
 print(myTown.citizens)
 print("\(myTown.name) has \(myTown.resources["followers"]!) followers.")
 myTown.citizens.append("Denise")
 print(myTown.citizens.count)
+myTown.film()
